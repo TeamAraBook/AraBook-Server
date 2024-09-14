a959409c9e74a5f581e45b32002ee1b31d9a483c
sung-silver <jinna0319@gmail.com>
2024-09-15 01:15:04 +0900

    [Setting] #1 - 프로젝트 생성

---
+++ src/test/java/com/arabook/arabook/ArabookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arabook.arabook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArabookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
