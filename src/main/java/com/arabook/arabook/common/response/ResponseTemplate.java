5f3e5cca675229dd18bf4fc783a539a6bfb8800a
sung-silver <jinna0319@gmail.com>
2024-09-23 22:31:04 +0900

    [Feat] #5 - ResponseTemplate에 DEFAULT_DATA 추가

--- src/main/java/com/arabook/arabook/common/response/ResponseTemplate.java
+++ src/main/java/com/arabook/arabook/common/response/ResponseTemplate.java
@@ -3,28 +3,27 @@ package com.arabook.arabook.common.response;
 import com.arabook.arabook.common.exception.common.ExceptionType;
 import com.arabook.arabook.common.success.common.SuccessType;
 
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor
 public class ResponseTemplate<T> {
+	private static final String DEFAULT_DATA = "No Data";
+
 	private final int code;
 	private final String message;
 	private T data;
 
 	public static ResponseTemplate success(SuccessType type) {
-		return new ResponseTemplate<>(type.status().value(), type.message());
+		return new ResponseTemplate<>(type.status().value(), type.message(), DEFAULT_DATA);
 	}
 
 	public static <T> ResponseTemplate<T> success(SuccessType type, T data) {
-		return new ResponseTemplate<T>(type.status().value(), type.message(), data);
+		return new ResponseTemplate<>(type.status().value(), type.message(), data);
 	}
 
 	public static ResponseTemplate error(ExceptionType type) {
-		return new ResponseTemplate<>(type.status().value(), type.message());
+		return new ResponseTemplate<>(type.status().value(), type.message(), DEFAULT_DATA);
 	}
 }
