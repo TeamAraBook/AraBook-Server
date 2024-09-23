030654f20df9b2dc8e7105927fa1599a8ee4ad47
sung-silver <jinna0319@gmail.com>
2024-09-23 20:32:46 +0900

    [Chore] #5 - formatter 속성 수정에 따른 코드 재정렬

--- src/main/java/com/arabook/arabook/ArabookApplication.java
+++ src/main/java/com/arabook/arabook/ArabookApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ArabookApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(ArabookApplication.class, args);
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(ArabookApplication.class, args);
+	}
 }
