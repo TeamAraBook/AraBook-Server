50d48bf709841514242c3bfb5455a18d1a2baddf
sung-silver <jinna0319@gmail.com>
2024-09-23 20:55:18 +0900

    [Add] #5 - CommonExceptionType 추가

--- src/main/java/com/arabook/arabook/common/exception/common/CommonExceptionType.java
+++ src/main/java/com/arabook/arabook/common/exception/common/CommonExceptionType.java
@@ -11,6 +11,8 @@ import lombok.RequiredArgsConstructor;
 public enum CommonExceptionType implements ExceptionType {
 	// 400 Bad Request
 	INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "입력값이 올바르지 않습니다"),
+	INVALID_REQUEST_PARAM_TYPE(HttpStatus.BAD_REQUEST, "요청한 파라미터 타입이 올바르지 않습니다"),
+	NOT_NULL_REQUEST_PARAM(HttpStatus.BAD_REQUEST, "필수 요청 파라미터가 누락되었습니다"),
 	// 404 Not Found
 	NOT_FOUND_PATH(HttpStatus.NOT_FOUND, "요청하신 경로를 찾을 수 없습니다"),
 	// 415 UNSUPPORTED_MEDIA_TYPE
