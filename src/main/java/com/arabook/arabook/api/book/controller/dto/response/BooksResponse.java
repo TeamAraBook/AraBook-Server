7792f0ec4340871692a05e299fd43095bceecf42
sung-silver <jinna0319@gmail.com>
2024-09-27 04:53:00 +0900

    [FEAT] #7 - 책 리스트 조회 응답 값 Swagger 명세 작성

--- src/main/java/com/arabook/arabook/api/book/controller/dto/response/BooksResponse.java
+++ src/main/java/com/arabook/arabook/api/book/controller/dto/response/BooksResponse.java
@@ -2,4 +2,9 @@ package com.arabook.arabook.api.book.controller.dto.response;
 
 import java.util.List;
 
-public record BooksResponse(int tatalCount, List<BookResponse> books) {}
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(name = "BooksResponse", description = "책 리스트 응답 DTO")
+public record BooksResponse(
+		@Schema(description = "조회된 책의 수", example = "1") int totalCount,
+		@Schema(description = "책 목록", example = "1") List<BookResponse> books) {}
