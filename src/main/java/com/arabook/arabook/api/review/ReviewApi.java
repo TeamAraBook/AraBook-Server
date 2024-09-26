a2f11366efe47b8782b414b9be446ca852174c9b
sung-silver <jinna0319@gmail.com>
2024-09-27 04:26:09 +0900

    [FEAT] #7 - 책 기록 생성 API 명세서 작성

---
+++ src/main/java/com/arabook/arabook/api/review/ReviewApi.java
@@ -0,0 +1,22 @@
+package com.arabook.arabook.api.review;
+
+import org.springframework.http.ResponseEntity;
+
+import com.arabook.arabook.common.response.ResponseTemplate;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Review", description = "책 기록 관련 API")
+public interface ReviewApi {
+	@ApiResponses(
+			value = {
+				@ApiResponse(responseCode = "200", description = "기록이 생성되었습니다."),
+				@ApiResponse(responseCode = "400", description = "기록 생성 요청이 잘못되었습니다."),
+				@ApiResponse(responseCode = "404", description = "해당 책을 찾을 수 없습니다.")
+			})
+	@Operation(summary = "책 기록하기: 책 기록 생성 요청", description = "책 기록을 생성합니다.")
+	ResponseEntity<ResponseTemplate> createReview(CreateReviewRequest request);
+}
