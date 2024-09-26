a91524335fa4e78c13c6d6b3ad6a18951ff7b164
sung-silver <jinna0319@gmail.com>
2024-09-27 03:32:55 +0900

    [Feat] #7 - 책 상세 조회 API 명세서 작성

--- src/main/java/com/arabook/arabook/api/book/BookApi.java
+++ src/main/java/com/arabook/arabook/api/book/BookApi.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 
 import com.arabook.arabook.api.book.response.AIRecommendBookResponse;
+import com.arabook.arabook.api.book.response.BookDetailResponse;
 import com.arabook.arabook.api.book.response.BookResponse;
 import com.arabook.arabook.common.response.ResponseTemplate;
 
@@ -22,4 +23,8 @@ public interface BookApi {
 	@ApiResponses(value = {@ApiResponse(responseCode = "200", description = "베스트셀러 책 조회에 성공했습니다.")})
 	@Operation(summary = "홈: 이달의 베스트셀러 조회", description = "이달의 베스트셀러를 조회합니다.")
 	ResponseEntity<ResponseTemplate<List<BookResponse>>> getBestSellerBooks();
+
+	@ApiResponses(value = {@ApiResponse(responseCode = "200", description = "책 상세보기 조회에 성공했습니다.")})
+	@Operation(summary = "책 상세보기: 책 정보 조회", description = "책 정보를 조회합니다.")
+	ResponseEntity<ResponseTemplate<BookDetailResponse>> getBookDetail(Long bookId);
 }
---
+++ src/main/java/com/arabook/arabook/api/book/response/BookDetailResponse.java
@@ -0,0 +1,16 @@
+package com.arabook.arabook.api.book.response;
+
+import java.util.List;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record BookDetailResponse(
+		@Schema(description = "책 id", example = "1") long bookId,
+		@Schema(description = "책 표지 url", example = "https://images/1") String coverUrl,
+		@Schema(description = "책 제목", example = "퀸의 대각선") String title,
+		@Schema(description = "책 작가", example = "베르나르베르베르") String author,
+		@Schema(description = "책 출판사", example = "출판사") String publisher,
+		@Schema(description = "책 출판일", example = "2021-01-01") String publicationDate,
+		@Schema(description = "책 설명", example = "책 설명") String description,
+		@Schema(description = "책 카테고리") List<CategoryResponse> categories,
+		@Schema(description = "책 해시태그") List<HashTagResponse> hashtags) {}
