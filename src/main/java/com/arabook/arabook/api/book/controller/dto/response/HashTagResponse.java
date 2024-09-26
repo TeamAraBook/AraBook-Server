7fe4c0d955257472beba2fd575b594605fadf9c3
sung-silver <jinna0319@gmail.com>
2024-09-27 04:55:26 +0900

    [FEAT] #7 - 책 해시태그 응답 값 Swagger 명세 작성

--- src/main/java/com/arabook/arabook/api/book/controller/dto/response/HashTagResponse.java
+++ src/main/java/com/arabook/arabook/api/book/controller/dto/response/HashTagResponse.java
@@ -1,3 +1,8 @@
 package com.arabook.arabook.api.book.controller.dto.response;
 
-public record HashTagResponse(long hashTagId, String name) {}
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(name = "HashTagResponse", description = "책 해시태그 응답 DTO")
+public record HashTagResponse(
+		@Schema(description = "해시태그 id", example = "1") long hashTagId,
+		@Schema(description = "해시태그 이름", example = "감동") String name) {}
