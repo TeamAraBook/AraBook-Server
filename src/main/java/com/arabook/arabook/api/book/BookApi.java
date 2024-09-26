0ac9e087886b982e61a902e272f6143123566775
sung-silver <jinna0319@gmail.com>
2024-09-27 03:21:48 +0900

    [Feat] #7 - 이달의 베스트셀러 조회 API 명세서 작성

--- src/main/java/com/arabook/arabook/api/book/BookApi.java
+++ src/main/java/com/arabook/arabook/api/book/BookApi.java
@@ -1,8 +1,11 @@
 package com.arabook.arabook.api.book;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 
 import com.arabook.arabook.api.book.response.AIRecommendBookResponse;
+import com.arabook.arabook.api.book.response.BookResponse;
 import com.arabook.arabook.common.response.ResponseTemplate;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -15,4 +18,8 @@ public interface BookApi {
 	@ApiResponses(value = {@ApiResponse(responseCode = "200", description = "AI 추천 책 조회에 성공했습니다.")})
 	@Operation(summary = "홈: AI 추천도서 ", description = "AI 추천도서를 조회합니다.")
 	ResponseEntity<ResponseTemplate<AIRecommendBookResponse>> getAIRecommendBook();
+
+	@ApiResponses(value = {@ApiResponse(responseCode = "200", description = "베스트셀러 책 조회에 성공했습니다.")})
+	@Operation(summary = "홈: 이달의 베스트셀러 조회", description = "이달의 베스트셀러를 조회합니다.")
+	ResponseEntity<ResponseTemplate<List<BookResponse>>> getBestSellerBooks();
 }
