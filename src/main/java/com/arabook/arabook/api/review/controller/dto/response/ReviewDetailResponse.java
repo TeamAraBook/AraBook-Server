c7cc183aa0d12931c6e678645050bc90a35ed8e7
sung-silver <jinna0319@gmail.com>
2024-09-27 05:09:01 +0900

    [FEAT] #7 - 책 기록 상세 조회 응답값 Swagger 명세 작성

--- src/main/java/com/arabook/arabook/api/review/controller/dto/response/ReviewDetailResponse.java
+++ src/main/java/com/arabook/arabook/api/review/controller/dto/response/ReviewDetailResponse.java
@@ -2,15 +2,18 @@ package com.arabook.arabook.api.review.controller.dto.response;
 
 import java.time.LocalDate;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(name = "ReviewDetailResponse", description = "책에 대한 기록 상세 조회 DTO")
 public record ReviewDetailResponse(
-		long reviewId,
-		long bookId,
-		LocalDate readStartDate,
-		LocalDate readEndDate,
-		String coverUrl,
-		String title,
-		String author,
-		String reviewTagIcon,
-		String reviewTagComment,
-		String reviewTagColor,
-		String content) {}
+		@Schema(description = "기록 id", example = "1") long reviewId,
+		@Schema(description = "책 id", example = "1") long bookId,
+		@Schema(description = "책을 읽기 시작한 날", example = "2024-09-21") LocalDate readStartDate,
+		@Schema(description = "책을 다 읽은 날", example = "2024-09-24") LocalDate readEndDate,
+		@Schema(description = "책 표지 url", example = "https://images/1") String coverUrl,
+		@Schema(description = "책 제목", example = "퀸의 대각선") String title,
+		@Schema(description = "책 작가", example = "베르나르베르베르") String author,
+		@Schema(description = "기록 캐릭터 icon", example = "https://icon/1") String reviewTagIcon,
+		@Schema(description = "기록 캐릭터 comment", example = "조금 아쉬웠어") String reviewTagComment,
+		@Schema(description = "기록 캐릭터 color", example = "1FD068") String reviewTagColor,
+		@Schema(description = "기록 내용", example = "재밌었음") String content) {}
