a518c099dea6ce061360e8f109ad0053f2086ac4
sung-silver <jinna0319@gmail.com>
2024-10-01 22:55:35 +0900

    [Feat] #17 - BaseTimeEntity 구현

---
+++ src/main/java/com/arabook/arabook/storage/domain/common/entity/BaseTimeEntity.java
@@ -0,0 +1,20 @@
+package com.arabook.arabook.storage.domain.common.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class BaseTimeEntity {
+	@CreatedDate protected LocalDateTime createdAt;
+	@LastModifiedDate protected LocalDateTime updatedAt;
+}
