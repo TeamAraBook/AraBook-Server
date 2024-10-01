de95a07a6bc94027e3bd7b986022a255934189dc
sung-silver <jinna0319@gmail.com>
2024-10-01 23:43:53 +0900

    [Feat] #17 - BookCategoryMapping 엔티티 구현

---
+++ src/main/java/com/arabook/arabook/storage/domain/book/entity/BookCategoryMapping.java
@@ -0,0 +1,31 @@
+package com.arabook.arabook.storage.domain.book.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import com.arabook.arabook.storage.domain.category.entity.Category;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "book_category_mappings")
+public class BookCategoryMapping {
+	@Id @GeneratedValue private Long bookCategoryMappingId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "book_id")
+	private Book book;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "category_id")
+	private Category category;
+}
