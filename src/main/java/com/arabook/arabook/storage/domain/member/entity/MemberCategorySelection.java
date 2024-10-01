763e1bb75f3697458ad2a33cc550f88022ed8d48
sung-silver <jinna0319@gmail.com>
2024-10-01 23:38:09 +0900

    [Feat] #17 - MemberCategorySelection 엔티티 구현

---
+++ src/main/java/com/arabook/arabook/storage/domain/member/entity/MemberCategorySelection.java
@@ -0,0 +1,41 @@
+package com.arabook.arabook.storage.domain.member.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import com.arabook.arabook.storage.domain.category.entity.Category;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "member_category_selections")
+public class MemberCategorySelection {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long memberCategorySelectionId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "category_id")
+	private Category category;
+
+	@Builder
+	private MemberCategorySelection(Member member, Category category) {
+		this.member = member;
+		this.category = category;
+	}
+}
