736d6f9290f179744707e73896cb13e726302652
sung-silver <jinna0319@gmail.com>
2024-10-02 00:22:00 +0900

    [Feat] #17 - BestSeller 엔티티 구현

---
+++ src/main/java/com/arabook/arabook/storage/domain/book/entity/BestSeller.java
@@ -0,0 +1,28 @@
+package com.arabook.arabook.storage.domain.book.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "monthly_best_sellers")
+public class BestSeller {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long bestSellerId;
+
+	@OneToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "book_isbn", referencedColumnName = "isbn", unique = true)
+	private Book book;
+}
