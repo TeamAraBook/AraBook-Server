7f892e637ec4e9bedbeaa0f5e4981b394ea97411
sung-silver <jinna0319@gmail.com>
2024-10-01 22:46:22 +0900

    [Feat] #17 - Book 엔티티 구현

---
+++ src/main/java/com/arabook/arabook/storage/domain/book/entity/Book.java
@@ -0,0 +1,68 @@
+package com.arabook.arabook.storage.domain.book.entity;
+
+import java.time.Year;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "books")
+public class Book {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Long bookId;
+
+	@NotNull
+	@Column(length = 13, unique = true)
+	String isbn;
+
+	@NotNull String title;
+
+	@NotNull String author;
+
+	@NotNull String coverUrl;
+
+	@NotNull Year publishYear;
+
+	@NotNull String publisher;
+
+	@NotNull String description;
+
+	@Builder
+	private Book(
+			String isbn,
+			String title,
+			String author,
+			String coverUrl,
+			Year publishYear,
+			String publisher,
+			String description) {
+		this.isbn = validateIsbn(isbn);
+		this.title = title;
+		this.author = author;
+		this.coverUrl = coverUrl;
+		this.publishYear = publishYear;
+		this.publisher = publisher;
+		this.description = description;
+	}
+
+	private String validateIsbn(String isbn) {
+		// TODO:: ISBN 유효성 검사에 대한 예외 customException으로 변경
+		if (isbn.length() != 13) {
+			throw new IllegalArgumentException("ISBN은 13자리여야 합니다.");
+		}
+		return isbn;
+	}
+}
