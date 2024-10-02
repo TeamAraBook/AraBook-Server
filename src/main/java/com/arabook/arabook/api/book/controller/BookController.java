9f25f5321fc26433b4b52381b660ff3b940e33a3
sung-silver <jinna0319@gmail.com>
2024-10-03 05:36:52 +0900

    [Feat] #25 - BookController에 getBookDetail 구현 및 Swaager 명세가 있는 인터페이스 implement

--- src/main/java/com/arabook/arabook/api/book/controller/BookController.java
+++ src/main/java/com/arabook/arabook/api/book/controller/BookController.java
@@ -4,10 +4,12 @@ import static com.arabook.arabook.common.success.book.BookSuccessType.*;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.arabook.arabook.api.book.controller.dto.response.BookDetailResponse;
 import com.arabook.arabook.api.book.controller.dto.response.BooksResponse;
 import com.arabook.arabook.api.book.service.BookService;
 import com.arabook.arabook.common.response.ResponseTemplate;
@@ -17,9 +19,18 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/books")
 @RequiredArgsConstructor
-public class BookController {
+public class BookController implements BookApi {
   private final BookService bookService;
 
+  @Override
+  @GetMapping("/{bookId}")
+  public ResponseEntity<ResponseTemplate<BookDetailResponse>> getBookDetail(
+      @PathVariable("bookId") final Long bookId) {
+    BookDetailResponse response = bookService.getBookDetail(bookId);
+    return ResponseEntity.ok(ResponseTemplate.success(GET_BOOK_DETAIL, response));
+  }
+
+  @Override
   @GetMapping("/search")
   public ResponseEntity<ResponseTemplate<BooksResponse>> getBooksBySearch(
       @RequestParam("keyword") final String keyword) {
