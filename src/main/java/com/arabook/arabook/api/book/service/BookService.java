f2e2039272ae6bf63bd500800f4c009e4e206ac4
sung-silver <jinna0319@gmail.com>
2024-10-03 05:35:35 +0900

    [Feat] #25 - BookService 인터페이스에 getBookDetail 구현

--- src/main/java/com/arabook/arabook/api/book/service/BookService.java
+++ src/main/java/com/arabook/arabook/api/book/service/BookService.java
@@ -1,7 +1,10 @@
 package com.arabook.arabook.api.book.service;
 
+import com.arabook.arabook.api.book.controller.dto.response.BookDetailResponse;
 import com.arabook.arabook.api.book.controller.dto.response.BooksResponse;
 
 public interface BookService {
   BooksResponse getBooksBySearch(String keyword);
+
+  BookDetailResponse getBookDetail(Long bookId);
 }
