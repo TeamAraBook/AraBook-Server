82db9c45a6811022d4e8c034443ba8d74aa858c4
sung-silver <jinna0319@gmail.com>
2024-10-03 04:14:38 +0900

    [Feat] #23 - BookCustomRepository 인터페이스 구현

---
+++ src/main/java/com/arabook/arabook/storage/domain/book/repository/BookCustomRepository.java
@@ -0,0 +1,9 @@
+package com.arabook.arabook.storage.domain.book.repository;
+
+import java.util.List;
+
+import com.arabook.arabook.api.book.controller.dto.response.BookResponse;
+
+public interface BookCustomRepository {
+	List<BookResponse> findSearchedBooks(String searchKeyword);
+}
