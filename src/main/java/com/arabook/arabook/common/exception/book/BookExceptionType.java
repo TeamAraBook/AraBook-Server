49b4b7e2c0c0bf3380be4f2f26700672381a9286
sung-silver <jinna0319@gmail.com>
2024-10-03 03:16:40 +0900

    [Feat] #21 - Book 엔티티에 CustomException 적용

---
+++ src/main/java/com/arabook/arabook/common/exception/book/BookExceptionType.java
@@ -0,0 +1,26 @@
+package com.arabook.arabook.common.exception.book;
+
+import org.springframework.http.HttpStatus;
+
+import com.arabook.arabook.common.exception.common.ExceptionType;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public enum BookExceptionType implements ExceptionType {
+	INVALID_BOOK_ISBN(HttpStatus.BAD_REQUEST, "ISBN은 13자리여야 합니다");
+
+	private final HttpStatus status;
+	private final String message;
+
+	@Override
+	public HttpStatus status() {
+		return this.status;
+	}
+
+	@Override
+	public String message() {
+		return this.message;
+	}
+}
