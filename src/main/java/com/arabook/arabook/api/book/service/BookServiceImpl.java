3399850eb7b23b5f085c2c27f71eaca73cd4bb29
sung-silver <jinna0319@gmail.com>
2024-10-03 04:20:13 +0900

    [Feat] #23 - BookService 구현체인 BookServiceImpl에 getBooksBySearch 구현

---
+++ src/main/java/com/arabook/arabook/api/book/service/BookServiceImpl.java
@@ -0,0 +1,25 @@
+package com.arabook.arabook.api.book.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.arabook.arabook.api.book.controller.dto.response.BookResponse;
+import com.arabook.arabook.api.book.controller.dto.response.BooksResponse;
+import com.arabook.arabook.storage.domain.book.repository.BookRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class BookServiceImpl implements BookService {
+	private final BookRepository bookRepository;
+
+	@Override
+	public BooksResponse getBooksBySearch(String keyword) {
+		final List<BookResponse> bookResponses = bookRepository.findSearchedBooks(keyword);
+		return BooksResponse.of(bookResponses.size(), bookResponses);
+	}
+}
