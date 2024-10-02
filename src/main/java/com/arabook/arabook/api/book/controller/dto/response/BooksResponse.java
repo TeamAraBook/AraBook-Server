4a32b2539c4dbd35fe6133d6e6f544cafd613f04
sung-silver <jinna0319@gmail.com>
2024-10-03 04:18:33 +0900

    [Feat] #23 - BooksResponse에 static 메서드 of 구현

--- src/main/java/com/arabook/arabook/api/book/controller/dto/response/BooksResponse.java
+++ src/main/java/com/arabook/arabook/api/book/controller/dto/response/BooksResponse.java
@@ -7,4 +7,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @Schema(name = "BooksResponse", description = "책 리스트 응답 DTO")
 public record BooksResponse(
 		@Schema(description = "조회된 책의 수", example = "1") int totalCount,
-		@Schema(description = "책 목록", example = "1") List<BookResponse> books) {}
+		@Schema(description = "책 목록", example = "1") List<BookResponse> books) {
+	public static BooksResponse of(int totalCount, List<BookResponse> books) {
+		return new BooksResponse(totalCount, books);
+	}
+}
