9af589a2c2e82077ad5268279a088b79684eaabe
sung-silver <jinna0319@gmail.com>
2024-10-03 04:17:38 +0900

    [Feat] #23 - BookCustomRepository 구현체인 BookCustomRepoistoryImpl에 findSearchedBooks 구현

---
+++ src/main/java/com/arabook/arabook/storage/domain/book/repository/BookCustomRepositoryImpl.java
@@ -0,0 +1,38 @@
+package com.arabook.arabook.storage.domain.book.repository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.arabook.arabook.api.book.controller.dto.response.BookResponse;
+import com.arabook.arabook.storage.domain.book.entity.QBook;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class BookCustomRepositoryImpl implements BookCustomRepository {
+
+	private final JPAQueryFactory queryFactory;
+
+	@Override
+	public List<BookResponse> findSearchedBooks(String searchKeyword) {
+		QBook book = QBook.book;
+		BooleanBuilder builder = new BooleanBuilder();
+
+		builder.and(book.author.containsIgnoreCase(searchKeyword));
+		builder.and(book.title.containsIgnoreCase(searchKeyword));
+		builder.and(book.isbn.eq(searchKeyword));
+
+		return queryFactory
+				.select(
+						Projections.constructor(
+								BookResponse.class, book.bookId, book.coverUrl, book.title, book.author))
+				.from(book)
+				.where(builder)
+				.fetch();
+	}
+}
