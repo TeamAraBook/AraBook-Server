f1838cf9b8ffffaa6d6143746a038f7690a9daa3
sung-silver <jinna0319@gmail.com>
2024-10-03 04:28:22 +0900

    [Feat] #23 - BookApi 명세서에 RequestParam에 대한 description 추가

--- src/main/java/com/arabook/arabook/api/book/controller/BookApi.java
+++ src/main/java/com/arabook/arabook/api/book/controller/BookApi.java
@@ -7,6 +7,7 @@ import com.arabook.arabook.api.book.controller.dto.response.BooksResponse;
 import com.arabook.arabook.common.response.ResponseTemplate;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -25,5 +26,9 @@ public interface BookApi {
 	@Operation(
 			summary = "책 검색: 책 이름 또는 isbn 또는 저자 이름으로 조회",
 			description = "책 이름, isbn, 저자명 중 한가지로 책을 검색합니다.")
-	ResponseEntity<ResponseTemplate<BooksResponse>> getBooksBySearch(String search);
+	ResponseEntity<ResponseTemplate<BooksResponse>> getBooksBySearch(
+			@Parameter(
+							description = "검색할 책 이름, ISBN 또는 저자 이름 사용자가 입력값 없이 검색을 누를 경우 빈 문자열로 요청해야 함",
+							required = true)
+					String search);
 }
