1a5316059645aacb5d81efa00356c5078d43658a
sung-silver <jinna0319@gmail.com>
2024-10-03 04:35:31 +0900

    [Feat] #23 - BookController에 GetMapping 어노테이션 추가

--- src/main/java/com/arabook/arabook/api/book/controller/BookController.java
+++ src/main/java/com/arabook/arabook/api/book/controller/BookController.java
@@ -3,6 +3,7 @@ package com.arabook.arabook.api.book.controller;
 import static com.arabook.arabook.common.success.book.BookSuccessType.*;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 public class BookController {
   private final BookService bookService;
 
+  @GetMapping
   public ResponseEntity<ResponseTemplate<BooksResponse>> getBooksBySearch(
       @RequestParam("search") final String searchKeyword) {
     BooksResponse response = bookService.getBooksBySearch(searchKeyword);
