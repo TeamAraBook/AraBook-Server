84ad85812889d184fc4625828b1a0d3260b9bd52
sung-silver <jinna0319@gmail.com>
2024-10-03 03:19:56 +0900

    [Feat] #21 - Review 엔티티에 CustomException 적용

---
+++ src/main/java/com/arabook/arabook/common/exception/review/ReviewExceptionType.java
@@ -0,0 +1,27 @@
+package com.arabook.arabook.common.exception.review;
+
+import org.springframework.http.HttpStatus;
+
+import com.arabook.arabook.common.exception.common.ExceptionType;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public enum ReviewExceptionType implements ExceptionType {
+	INVALID_READ_START_DATE(HttpStatus.BAD_REQUEST, "책 읽기 시작 날짜가 올바르지 않습니다."),
+	;
+
+	private final HttpStatus status;
+	private final String message;
+
+	@Override
+	public HttpStatus status() {
+		return this.status;
+	}
+
+	@Override
+	public String message() {
+		return this.message;
+	}
+}
