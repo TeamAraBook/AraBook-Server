9a595327a7c671256f36ee5c63e3f8268d657dfe
sung-silver <jinna0319@gmail.com>
2024-10-04 00:18:51 +0900

    [Fix] #27 - example 수정

--- src/main/java/com/arabook/arabook/api/book/controller/BookApi.java
+++ src/main/java/com/arabook/arabook/api/book/controller/BookApi.java
@@ -8,6 +8,8 @@ import com.arabook.arabook.common.response.ResponseTemplate;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -16,8 +18,26 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 public interface BookApi {
   @ApiResponses(
       value = {
-        @ApiResponse(responseCode = "200", description = "책 상세보기 조회에 성공했습니다."),
-        @ApiResponse(responseCode = "404", description = "해당 책을 찾을 수 없습니다.")
+        @ApiResponse(
+            responseCode = "200",
+            description = "책 상세보기 조회에 성공했습니다.",
+            content =
+                @Content(
+                    mediaType = "application/json",
+                    schema =
+                        @Schema(
+                            example =
+                                "{\"code\": 200, \"message\": \"책 상세 정보를 조회하였습니다\", \"data\": { \"bookId\": 1, \"coverUrl\": \"https://images/1\", \"title\": \"사랑의 마법사는 외로워\", \"author\": \"김하늘\", \"publisher\": \"출판사 A\", \"publicationYear\": \"2022\", \"description\": \"사랑과 마법이 얽힌 판타지 소설입니다.\", \"categories\": [{ \"categoryId\": 1, \"name\": \"판타지\" }, { \"categoryId\": 2, \"name\": \"로맨스\" }], \"hashtags\": [{ \"hashTagId\": 1, \"name\": \"사랑\" }, { \"hashTagId\": 2, \"name\": \"마법\" }] }}"))),
+        @ApiResponse(
+            responseCode = "404",
+            description = "해당 책을 찾을 수 없습니다.",
+            content =
+                @Content(
+                    mediaType = "application/json",
+                    schema =
+                        @Schema(
+                            example =
+                                "{ \"code\": 404, \"message\": \"책을 찾을 수 없습니다.\", \"data\": \"No Data\" }")))
       })
   @Operation(summary = "책 상세보기: 책 정보 조회", description = "책 정보를 조회합니다.")
   ResponseEntity<ResponseTemplate<BookDetailResponse>> getBookDetail(Long bookId);
