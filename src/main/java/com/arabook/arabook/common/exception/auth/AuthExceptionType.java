737910816b8f0b7bd88fcc800b92bf7279aa9db8
sung-silver <jinna0319@gmail.com>
2024-10-05 04:01:19 +0900

    [Feat] #31 - AuthExceptionType 추가

--- src/main/java/com/arabook/arabook/common/exception/auth/AuthExceptionType.java
+++ src/main/java/com/arabook/arabook/common/exception/auth/AuthExceptionType.java
@@ -12,7 +12,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum AuthExceptionType implements ExceptionType {
   INVALID_TOKEN(HttpStatus.BAD_REQUEST, "서비스에서 발급되지 않은 토큰입니다."),
-  UNAUTHORIZED_TOKEN(HttpStatus.UNAUTHORIZED, "기한이 만료된 토큰입니다.");
+  UNAUTHORIZED_TOKEN(HttpStatus.UNAUTHORIZED, "기한이 만료된 토큰입니다."),
+  INVALID_REQUEST_LOGIN(HttpStatus.BAD_REQUEST, "올바르지 않은 로그인 요청입니다");
 
   private final HttpStatus status;
   private final String message;
