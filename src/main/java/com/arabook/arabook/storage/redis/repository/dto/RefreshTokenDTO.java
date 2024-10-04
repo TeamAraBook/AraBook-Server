2b4b5d9f1a7106b55c9748a7f691bc044f716507
sung-silver <jinna0319@gmail.com>
2024-10-05 04:00:14 +0900

    [Feat] #31 - RefreshTokenDTO 구현

---
+++ src/main/java/com/arabook/arabook/storage/redis/repository/dto/RefreshTokenDTO.java
@@ -0,0 +1,28 @@
+package com.arabook.arabook.storage.redis.repository.dto;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@RedisHash(value = "refresh", timeToLive = 604800016)
+public class RefreshTokenDTO {
+  @Id @Indexed private String memberId;
+
+  private String refreshToken;
+
+  public void updateRefreshToken(String refreshToken) {
+    this.refreshToken = refreshToken;
+  }
+
+  @Builder
+  private RefreshTokenDTO(String memberId, String refreshToken) {
+    this.memberId = memberId;
+    this.refreshToken = refreshToken;
+  }
+}
