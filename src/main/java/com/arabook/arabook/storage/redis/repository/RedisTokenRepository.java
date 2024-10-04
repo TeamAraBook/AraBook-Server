ee015a29af1cc89351ca4f7a9729421d3dadd038
sung-silver <jinna0319@gmail.com>
2024-10-05 04:00:43 +0900

    [Feat] #31 - RedisTokenRepository 구현

---
+++ src/main/java/com/arabook/arabook/storage/redis/repository/RedisTokenRepository.java
@@ -0,0 +1,20 @@
+package com.arabook.arabook.storage.redis.repository;
+
+import static com.arabook.arabook.common.exception.auth.AuthExceptionType.*;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.arabook.arabook.common.exception.auth.AuthException;
+import com.arabook.arabook.storage.redis.repository.dto.RefreshTokenDTO;
+
+@Repository
+public interface RedisTokenRepository extends CrudRepository<RefreshTokenDTO, String> {
+  Optional<RefreshTokenDTO> findByMemberId(String memberId);
+
+  default RefreshTokenDTO findByMemberIdOrElseThrowException(String memberId) {
+    return findByMemberId(memberId).orElseThrow(() -> new AuthException(UNAUTHORIZED_TOKEN));
+  }
+}
