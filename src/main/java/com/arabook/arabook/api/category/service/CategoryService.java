5500f2bc80445a61517449aee641a39fa3097f56
sung-silver <jinna0319@gmail.com>
2024-10-06 06:07:01 +0900

    [Feat] #41 - CategoryService 인터페이스 정의

---
+++ src/main/java/com/arabook/arabook/api/category/service/CategoryService.java
@@ -0,0 +1,7 @@
+package com.arabook.arabook.api.category.service;
+
+import com.arabook.arabook.api.category.controller.dto.response.CategoriesResponse;
+
+public interface CategoryService {
+  CategoriesResponse getCategories();
+}
