9bf1966575dcc8b532600d951e8ee87e4a7bfd41
sung-silver <jinna0319@gmail.com>
2024-10-06 04:56:36 +0900

    [Feat] #32 - MemberService 구현체인 MemberServiceImpl 구현

---
+++ src/main/java/com/arabook/arabook/api/member/service/MemberServiceImpl.java
@@ -0,0 +1,26 @@
+package com.arabook.arabook.api.member.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.arabook.arabook.api.member.controller.dto.request.MemberOnboardingRequest;
+import com.arabook.arabook.storage.domain.member.entity.Member;
+import com.arabook.arabook.storage.domain.member.repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberServiceImpl implements MemberService {
+  private final MemberRepository memberRepository;
+  private final MemberCategorySelectionService memberCategorySelectionSerivce;
+
+  @Override
+  @Transactional
+  public void onboarding(final MemberOnboardingRequest request, final Long memberId) {
+    Member member = memberRepository.findByMemberIdOrThrow(memberId);
+    member.updateOnboardingInfo(request.gender(), request.age());
+    memberCategorySelectionSerivce.selectCategories(member, request.interestCategoryIds());
+  }
+}
