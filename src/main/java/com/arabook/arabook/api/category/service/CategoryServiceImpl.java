f455a541f4008cb813714dbbd33b9b3364754501
sung-silver <jinna0319@gmail.com>
2024-10-06 06:07:54 +0900

    [Feat] #41 - CategoryService의 구현체인 CategoryServiceImpl에 카테고리 이름(오름차순) 순서대로 조회하는 기능 구현

---
+++ src/main/java/com/arabook/arabook/api/category/service/CategoryServiceImpl.java
@@ -0,0 +1,25 @@
+package com.arabook.arabook.api.category.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.arabook.arabook.api.category.controller.dto.response.CategoriesResponse;
+import com.arabook.arabook.api.category.controller.dto.response.CategoryResponse;
+import com.arabook.arabook.storage.domain.category.repository.CategoryRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class CategoryServiceImpl implements CategoryService {
+  private final CategoryRepository categoryRepository;
+
+  @Override
+  public CategoriesResponse getCategories() {
+    List<CategoryResponse> categories = categoryRepository.findAllOrderByCategoryNameAsc();
+    return CategoriesResponse.of(categories.size(), categories);
+  }
+}
