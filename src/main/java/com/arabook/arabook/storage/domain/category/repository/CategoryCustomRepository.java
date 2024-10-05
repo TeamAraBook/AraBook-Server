e91dcd28a938d05d8d45785714d2ab30508ca696
sung-silver <jinna0319@gmail.com>
2024-10-06 06:00:38 +0900

    [Feat] #41 - CategoryCustomRepository 인터페이스 정의

---
+++ src/main/java/com/arabook/arabook/storage/domain/category/repository/CategoryCustomRepository.java
@@ -0,0 +1,9 @@
+package com.arabook.arabook.storage.domain.category.repository;
+
+import java.util.List;
+
+import com.arabook.arabook.api.category.controller.dto.response.CategoryResponse;
+
+public interface CategoryCustomRepository {
+  List<CategoryResponse> findAllOrderByCategoryNameAsc();
+}
