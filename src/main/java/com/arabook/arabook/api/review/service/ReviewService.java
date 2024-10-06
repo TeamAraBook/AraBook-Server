42c8645550d6e5bb84fc2da939b0d8860baf04f4
sung-silver <jinna0319@gmail.com>
2024-10-06 18:49:15 +0900

    [Feat] #36 - ReviewService 인터페이스에 updateReview 정의

--- src/main/java/com/arabook/arabook/api/review/service/ReviewService.java
+++ src/main/java/com/arabook/arabook/api/review/service/ReviewService.java
@@ -1,7 +1,11 @@
 package com.arabook.arabook.api.review.service;
 
 import com.arabook.arabook.api.review.controller.dto.request.CreateReviewRequest;
+import com.arabook.arabook.api.review.controller.dto.request.UpdateReviewRequest;
+import com.arabook.arabook.api.review.controller.dto.response.ReviewIdResponse;
 
 public interface ReviewService {
-  Long createReview(CreateReviewRequest request, Long memberId);
+  ReviewIdResponse createReview(CreateReviewRequest request, Long memberId);
+
+  ReviewIdResponse updateReview(UpdateReviewRequest request, Long memberId);
 }
