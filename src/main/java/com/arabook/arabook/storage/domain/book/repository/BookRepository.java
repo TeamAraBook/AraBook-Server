685198aaa995d1470cb347ace6506ee14d06af84
sung-silver <jinna0319@gmail.com>
2024-10-06 18:14:26 +0900

    [Feat] #35 - BookRepository에 findByBookIdOrThrow 구현

--- src/main/java/com/arabook/arabook/storage/domain/book/repository/BookRepository.java
+++ src/main/java/com/arabook/arabook/storage/domain/book/repository/BookRepository.java
@@ -1,7 +1,18 @@
 package com.arabook.arabook.storage.domain.book.repository;
 
+import static com.arabook.arabook.common.exception.book.BookExceptionType.*;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.arabook.arabook.common.exception.book.BookException;
 import com.arabook.arabook.storage.domain.book.entity.Book;
 
-public interface BookRepository extends JpaRepository<Book, Long>, BookCustomRepository {}
+public interface BookRepository extends JpaRepository<Book, Long>, BookCustomRepository {
+  Optional<Book> findByBookId(Long bookId);
+
+  default Book findByBookIdOrThrow(Long bookId) {
+    return findByBookId(bookId).orElseThrow(() -> new BookException(BOOK_NOT_FOUND));
+  }
+}
