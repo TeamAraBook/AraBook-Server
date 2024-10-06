f936a27df000f8b2ec8780c128f5b326e542c2d9
sung-silver <jinna0319@gmail.com>
2024-10-06 16:35:51 +0900

    [Feat] AI 추천 도서 조회 API 구현

--- src/main/java/com/arabook/arabook/api/book/controller/RecommendController.java
+++ src/main/java/com/arabook/arabook/api/book/controller/RecommendController.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.arabook.arabook.api.book.controller.dto.response.AIRecommendBookResponse;
 import com.arabook.arabook.api.book.controller.dto.response.BooksResponse;
 import com.arabook.arabook.api.book.service.BestSellerService;
+import com.arabook.arabook.api.global.security.AuthMember;
 import com.arabook.arabook.common.response.ResponseTemplate;
 
 import lombok.RequiredArgsConstructor;
@@ -16,12 +18,21 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/recommend")
 @RequiredArgsConstructor
-public class RecommendController {
+public class RecommendController implements RecommendApi {
   private final BestSellerService bestSellerService;
 
+  @Override
   @GetMapping("/best-seller")
   public ResponseEntity<ResponseTemplate<BooksResponse>> getBestSellerBooks() {
     BooksResponse response = bestSellerService.getBestSellerBooks();
     return ResponseEntity.ok(ResponseTemplate.success(GET_BEST_SELLER_BOOKS, response));
+  }
+
+  @Override
+  @GetMapping("/ai")
+  public ResponseEntity<ResponseTemplate<AIRecommendBookResponse>> getAIRecommendBook(
+      @AuthMember final Long memberId) {
+    AIRecommendBookResponse response = bestSellerService.getAIRecommendBook(memberId);
+    return ResponseEntity.ok(ResponseTemplate.success(GET_AI_BOOK, response));
   }
 }
