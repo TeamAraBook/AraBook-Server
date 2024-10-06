f9a3fb0af14e6837e88f2984f57387e9a2301dbd
sung-silver <jinna0319@gmail.com>
2024-10-06 20:00:50 +0900

    [Feat] #33 - ReviewService에 getReviews 정의

--- src/main/java/com/arabook/arabook/api/review/service/ReviewService.java
+++ src/main/java/com/arabook/arabook/api/review/service/ReviewService.java
@@ -3,9 +3,12 @@ package com.arabook.arabook.api.review.service;
 import com.arabook.arabook.api.review.controller.dto.request.CreateReviewRequest;
 import com.arabook.arabook.api.review.controller.dto.request.UpdateReviewRequest;
 import com.arabook.arabook.api.review.controller.dto.response.ReviewIdResponse;
+import com.arabook.arabook.api.review.controller.dto.response.ReviewsResponse;
 
 public interface ReviewService {
   ReviewIdResponse createReview(CreateReviewRequest request, Long memberId);
 
   ReviewIdResponse updateReview(UpdateReviewRequest request, Long memberId);
+
+  ReviewsResponse getReviews(Long memberId);
 }
