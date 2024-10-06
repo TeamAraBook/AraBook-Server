47d04448b6076e8ca7ab78577d1b31cb04fb0d55
sung-silver <jinna0319@gmail.com>
2024-10-06 18:48:32 +0900

    [Feat] #36 - ReviewRepository에 findByReviewIdAndReviewerIdOrThrow 구현

--- src/main/java/com/arabook/arabook/storage/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/arabook/arabook/storage/domain/review/repository/ReviewRepository.java
@@ -1,7 +1,23 @@
 package com.arabook.arabook.storage.domain.review.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import static com.arabook.arabook.common.exception.review.ReviewExceptionType.*;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.arabook.arabook.common.exception.review.ReviewException;
 import com.arabook.arabook.storage.domain.review.entity.Review;
 
-public interface ReviewRepository extends JpaRepository<Review, Long> {}
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+  @Query("SELECT r FROM Review r WHERE r.reviewId = :reviewId AND r.reviwer.memberId = :reviewerId")
+  Optional<Review> findByReviewIdAndReviewerId(
+      @Param("reviewId") final Long reviewId, @Param("reviewerId") final Long reviewerId);
+
+  default Review findByReviewIdAndReviewerIdOrThrow(final Long reviewId, final Long reviewerId) {
+    return findByReviewIdAndReviewerId(reviewId, reviewerId)
+        .orElseThrow(() -> new ReviewException(NOT_FOUND_REVIEW));
+  }
+}
