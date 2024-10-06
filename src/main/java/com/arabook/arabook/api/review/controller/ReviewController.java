46344c889908f53d157214ae97431c48ded8ecf6
sung-silver <jinna0319@gmail.com>
2024-10-06 20:01:50 +0900

    [Feat] #33 - 기록 목록 조회 API 구현

--- src/main/java/com/arabook/arabook/api/review/controller/ReviewController.java
+++ src/main/java/com/arabook/arabook/api/review/controller/ReviewController.java
@@ -7,6 +7,7 @@ import java.net.URI;
 import jakarta.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import com.arabook.arabook.api.global.security.AuthMember;
 import com.arabook.arabook.api.review.controller.dto.request.CreateReviewRequest;
 import com.arabook.arabook.api.review.controller.dto.request.UpdateReviewRequest;
 import com.arabook.arabook.api.review.controller.dto.response.ReviewIdResponse;
+import com.arabook.arabook.api.review.controller.dto.response.ReviewsResponse;
 import com.arabook.arabook.api.review.service.ReviewService;
 import com.arabook.arabook.common.response.ResponseTemplate;
 
@@ -44,5 +46,11 @@ public class ReviewController {
       @RequestBody @Valid UpdateReviewRequest request, @AuthMember final Long memberId) {
     ReviewIdResponse response = reviewService.updateReview(request, memberId);
     return ResponseEntity.ok().body(ResponseTemplate.success(UPDATE_REVIEW_SUCCESS, response));
+  }
+
+  @GetMapping("")
+  ResponseEntity<ResponseTemplate<ReviewsResponse>> getReviews(@AuthMember final Long memberId) {
+    ReviewsResponse response = reviewService.getReviews(memberId);
+    return ResponseEntity.ok().body(ResponseTemplate.success(GET_REVIEWS_SUCCESS, response));
   }
 }
