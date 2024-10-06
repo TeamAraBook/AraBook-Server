1addaa52cda784d6e469a4ea0cf805a28e662b15
sung-silver <jinna0319@gmail.com>
2024-10-06 18:50:50 +0900

    [Feat] #36 - 책 기록 수정 API 구현

--- src/main/java/com/arabook/arabook/api/review/controller/ReviewController.java
+++ src/main/java/com/arabook/arabook/api/review/controller/ReviewController.java
@@ -8,12 +8,15 @@ import jakarta.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.arabook.arabook.api.global.security.AuthMember;
 import com.arabook.arabook.api.review.controller.dto.request.CreateReviewRequest;
+import com.arabook.arabook.api.review.controller.dto.request.UpdateReviewRequest;
+import com.arabook.arabook.api.review.controller.dto.response.ReviewIdResponse;
 import com.arabook.arabook.api.review.service.ReviewService;
 import com.arabook.arabook.common.response.ResponseTemplate;
 
@@ -28,10 +31,18 @@ public class ReviewController {
   private static final String REVIEW_PATH = "/reviews";
 
   @PostMapping("")
-  public ResponseEntity<ResponseTemplate> createReview(
+  public ResponseEntity<ResponseTemplate<ReviewIdResponse>> createReview(
       @RequestBody @Valid final CreateReviewRequest request, @AuthMember final Long memberId) {
-    Long reviewId = reviewService.createReview(request, memberId);
-    URI uri = URI.create(REVIEW_PATH + "/" + reviewId);
-    return ResponseEntity.created(uri).body(ResponseTemplate.success(CREATE_REVIEW_SUCCESS));
+    ReviewIdResponse response = reviewService.createReview(request, memberId);
+    URI uri = URI.create(REVIEW_PATH + "/" + response.reviewId());
+    return ResponseEntity.created(uri)
+        .body(ResponseTemplate.success(CREATE_REVIEW_SUCCESS, response));
+  }
+
+  @PutMapping("")
+  ResponseEntity<ResponseTemplate<ReviewIdResponse>> updateReview(
+      @RequestBody @Valid UpdateReviewRequest request, @AuthMember final Long memberId) {
+    ReviewIdResponse response = reviewService.updateReview(request, memberId);
+    return ResponseEntity.ok().body(ResponseTemplate.success(UPDATE_REVIEW_SUCCESS, response));
   }
 }
