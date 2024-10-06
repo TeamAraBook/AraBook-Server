d6d5e60f5014719bf0f12e4b5c55b8f4a19cbc2b
sung-silver <jinna0319@gmail.com>
2024-10-06 21:07:01 +0900

    [Feat] #46 - ReviewCustomRepository에 findReviewDetailByReviewIdAndReviewerId 정의

--- src/main/java/com/arabook/arabook/storage/domain/review/repository/ReviewCustomRepository.java
+++ src/main/java/com/arabook/arabook/storage/domain/review/repository/ReviewCustomRepository.java
@@ -2,8 +2,11 @@ package com.arabook.arabook.storage.domain.review.repository;
 
 import java.util.List;
 
+import com.arabook.arabook.api.review.controller.dto.response.ReviewDetailResponse;
 import com.arabook.arabook.api.review.controller.dto.response.ReviewResponse;
 
 public interface ReviewCustomRepository {
   List<ReviewResponse> findReviewsByReviewerId(Long memberId);
+
+  ReviewDetailResponse findReviewDetailByReviewIdAndReviewerId(Long reviewId, Long memberId);
 }
