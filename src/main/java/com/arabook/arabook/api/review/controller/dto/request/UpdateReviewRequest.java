074c1eb964bf625cdf3403da29db02eb645c4df8
sung-silver <jinna0319@gmail.com>
2024-10-06 17:20:01 +0900

    [Feat] #35 - 책 기록하기/수정하기 request swagger example 변경

--- src/main/java/com/arabook/arabook/api/review/controller/dto/request/UpdateReviewRequest.java
+++ src/main/java/com/arabook/arabook/api/review/controller/dto/request/UpdateReviewRequest.java
@@ -13,7 +13,7 @@ public record UpdateReviewRequest(
         @Schema(
             description =
                 "리뷰 태그(DISAPPOINTED,SLIGHTLY_DISAPPOINTED,AVERAGE,ENJOYABLE,LIFE_CHANGING 중 1",
-            example = "1")
+            example = "DISAPPOINTED")
         String reviewTag,
     @NotNull @Schema(description = "기록 내용", example = "재밌었음") String content,
     @NotNull @Schema(description = "책을 읽기 시작한 날", example = "2024-09-21") LocalDate readStartDate,
