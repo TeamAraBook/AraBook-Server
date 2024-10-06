ed1748eca106a9041756502f17a43eed72fcc95f
sung-silver <jinna0319@gmail.com>
2024-10-06 20:01:50 +0900

    [Feat] #33 - 기록 목록 조회 API 구현

--- src/main/java/com/arabook/arabook/common/success/review/ReviewSuccessType.java
+++ src/main/java/com/arabook/arabook/common/success/review/ReviewSuccessType.java
@@ -9,7 +9,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ReviewSuccessType implements SuccessType {
   CREATE_REVIEW_SUCCESS(HttpStatus.CREATED, "기록이 생성되었습니다"),
-  UPDATE_REVIEW_SUCCESS(HttpStatus.OK, "기록을 수정했습니다");
+  UPDATE_REVIEW_SUCCESS(HttpStatus.OK, "기록을 수정했습니다"),
+  GET_REVIEWS_SUCCESS(HttpStatus.OK, "기록 목록을 조회했습니다");
 
   private final HttpStatus status;
   private final String message;
