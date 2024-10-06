8ae75818f722d565111ca3abdb1728ed9398c24f
sung-silver <jinna0319@gmail.com>
2024-10-06 18:50:26 +0900

    [Feat] #36 - 리뷰에 대한 Not Found 예외 추가

--- src/main/java/com/arabook/arabook/common/exception/review/ReviewExceptionType.java
+++ src/main/java/com/arabook/arabook/common/exception/review/ReviewExceptionType.java
@@ -10,7 +10,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum ReviewExceptionType implements ExceptionType {
   INVALID_READ_START_DATE(HttpStatus.BAD_REQUEST, "책 읽기 시작 날짜가 올바르지 않습니다."),
-  ;
+  NOT_FOUND_REVIEW(HttpStatus.NOT_FOUND, "기록을 찾을 수 없습니다");
 
   private final HttpStatus status;
   private final String message;
