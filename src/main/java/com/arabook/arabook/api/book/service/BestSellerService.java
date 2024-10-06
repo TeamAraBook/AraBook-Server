80c26a817521bbb59d00907e3c46ec03eccabbaf
sung-silver <jinna0319@gmail.com>
2024-10-06 16:34:34 +0900

    [Feat] #37 - BestSellerService에 getAIRecoomendBook 정의

--- src/main/java/com/arabook/arabook/api/book/service/BestSellerService.java
+++ src/main/java/com/arabook/arabook/api/book/service/BestSellerService.java
@@ -1,7 +1,10 @@
 package com.arabook.arabook.api.book.service;
 
+import com.arabook.arabook.api.book.controller.dto.response.AIRecommendBookResponse;
 import com.arabook.arabook.api.book.controller.dto.response.BooksResponse;
 
 public interface BestSellerService {
   BooksResponse getBestSellerBooks();
+
+  AIRecommendBookResponse getAIRecommendBook(Long memberId);
 }
