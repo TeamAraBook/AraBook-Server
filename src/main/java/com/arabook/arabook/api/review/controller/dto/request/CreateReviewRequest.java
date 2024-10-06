4da0202493a67ea20e66a1309998561ac07abd84
sung-silver <jinna0319@gmail.com>
2024-10-06 17:35:30 +0900

    [Feat] #35 - (Create,Update)ReviewRequest에 reviewTag를 String에서 ReviewTag 타입으로 변경

--- src/main/java/com/arabook/arabook/api/review/controller/dto/request/CreateReviewRequest.java
+++ src/main/java/com/arabook/arabook/api/review/controller/dto/request/CreateReviewRequest.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 
 import jakarta.validation.constraints.NotNull;
 
+import com.arabook.arabook.storage.domain.review.entity.enums.ReviewTag;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(name = "CreateReviewRequest", description = "책에 대한 기록 생성 요청 DTO")
@@ -14,7 +16,7 @@ public record CreateReviewRequest(
             description =
                 "리뷰 태그(DISAPPOINTED,SLIGHTLY_DISAPPOINTED,AVERAGE,ENJOYABLE,LIFE_CHANGING 중 1",
             example = "DISAPPOINTED")
-        String reviewTag,
+        ReviewTag reviewTag,
     @NotNull @Schema(description = "기록 내용", example = "재밌었음") String content,
     @NotNull @Schema(description = "책을 읽기 시작한 날", example = "2024-09-21") LocalDate readStartDate,
     @NotNull @Schema(description = "책을 다 읽은 날", example = "2024-09-24") LocalDate readEndDate) {}
