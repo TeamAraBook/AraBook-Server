ff74488c02c0af119f8b88900fc4602ae3446fe0
sung-silver <jinna0319@gmail.com>
2024-10-06 18:49:51 +0900

    [Feat] #36 - ReviewService 구현체 ReviewServiceImpl에 updateReview 구현

--- src/main/java/com/arabook/arabook/api/review/service/ReviewServiceImpl.java
+++ src/main/java/com/arabook/arabook/api/review/service/ReviewServiceImpl.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.arabook.arabook.api.review.controller.dto.request.CreateReviewRequest;
+import com.arabook.arabook.api.review.controller.dto.request.UpdateReviewRequest;
+import com.arabook.arabook.api.review.controller.dto.response.ReviewIdResponse;
 import com.arabook.arabook.storage.domain.book.entity.Book;
 import com.arabook.arabook.storage.domain.book.repository.BookRepository;
 import com.arabook.arabook.storage.domain.member.entity.Member;
@@ -22,19 +24,31 @@ public class ReviewServiceImpl implements ReviewService {
   private final ReviewRepository reviewRepository;
 
   @Override
-  public Long createReview(CreateReviewRequest request, Long memberId) {
+  @Transactional
+  public ReviewIdResponse createReview(CreateReviewRequest request, Long memberId) {
     Member member = memberRepository.findByMemberIdOrThrow(memberId);
     Book book = bookRepository.findByBookIdOrThrow(request.bookId());
     Review review =
         Review.builder()
             .reviwer(member)
             .book(book)
+            .content(request.content())
             .reviewTag(request.reviewTag())
             .readStartDate(request.readStartDate())
             .readEndDate(request.readEndDate())
             .build();
     reviewRepository.save(review);
 
-    return review.getReviewId();
+    return ReviewIdResponse.of(review.getReviewId());
+  }
+
+  @Override
+  @Transactional
+  public ReviewIdResponse updateReview(final UpdateReviewRequest request, final Long memberId) {
+    Review review =
+        reviewRepository.findByReviewIdAndReviewerIdOrThrow(request.reviewId(), memberId);
+    review.updateReview(
+        request.content(), request.reviewTag(), request.readStartDate(), request.readEndDate());
+    return ReviewIdResponse.of(review.getReviewId());
   }
 }
