e505742a541985e07c5ddfc806b3c6ab942839cb
sung-silver <jinna0319@gmail.com>
2024-10-06 20:59:41 +0900

    [Feat] #46 - Schema hidden=true 활성화 및 memberId 파라미터 누락된 부분 변경

--- src/main/java/com/arabook/arabook/api/review/controller/ReviewApi.java
+++ src/main/java/com/arabook/arabook/api/review/controller/ReviewApi.java
@@ -10,6 +10,7 @@ import com.arabook.arabook.api.review.controller.dto.response.ReviewsResponse;
 import com.arabook.arabook.common.response.ResponseTemplate;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -26,7 +27,8 @@ public interface ReviewApi {
         @ApiResponse(responseCode = "404", description = "해당 책을 찾을 수 없습니다.")
       })
   @Operation(summary = "책 기록하기: 책 기록 생성 요청", description = "책 기록을 생성합니다.")
-  ResponseEntity<ResponseTemplate<ReviewIdResponse>> createReview(CreateReviewRequest request);
+  ResponseEntity<ResponseTemplate<ReviewIdResponse>> createReview(
+      CreateReviewRequest request, @Schema(hidden = true) Long memberId);
 
   @ApiResponses(
       value = {
@@ -34,7 +36,8 @@ public interface ReviewApi {
         @ApiResponse(responseCode = "404", description = "기록을 찾을 수 없습니다.")
       })
   @Operation(summary = "책 기록하기: 책 기록 조회", description = "책 기록을 조회합니다.")
-  ResponseEntity<ResponseTemplate<ReviewDetailResponse>> getReviewDetail(Long reviewId);
+  ResponseEntity<ResponseTemplate<ReviewDetailResponse>> getReviewDetail(
+      Long reviewId, @Schema(hidden = true) Long memberId);
 
   @ApiResponses(
       value = {
@@ -47,7 +50,7 @@ public interface ReviewApi {
       })
   @Operation(summary = "책 기록하기: 책 기록 수정 요청", description = "책 기록을 수정합니다.")
   ResponseEntity<ResponseTemplate<ReviewIdResponse>> updateReview(
-      UpdateReviewRequest request, Long memberId);
+      UpdateReviewRequest request, @Schema(hidden = true) Long memberId);
 
   @ApiResponses(
       value = {
@@ -55,9 +58,11 @@ public interface ReviewApi {
         @ApiResponse(responseCode = "404", description = "기록을 찾을 수 없습니다.")
       })
   @Operation(summary = "책 기록하기: 책 기록 삭제 요청", description = "책 기록을 삭제합니다.")
-  ResponseEntity<ResponseTemplate> deleteReview(Long reviewId);
+  ResponseEntity<ResponseTemplate> deleteReview(
+      Long reviewId, @Schema(hidden = true) Long memberId);
 
   @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "기록 목록 조회했습니다.")})
   @Operation(summary = "책 기록하기: 책 기록 목록 조회", description = "책 기록 목록을 조회합니다.")
-  ResponseEntity<ResponseTemplate<ReviewsResponse>> getReviews(Long memberId);
+  ResponseEntity<ResponseTemplate<ReviewsResponse>> getReviews(
+      @Schema(hidden = true) Long memberId);
 }
