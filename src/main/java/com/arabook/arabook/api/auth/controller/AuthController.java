de901f8a9ea408270dbaaf35e1e53cd600d1bf9a
sung-silver <jinna0319@gmail.com>
2024-10-10 01:37:08 +0900

    [Feat] #51 - 소셜 로그인 API 구현

---
+++ src/main/java/com/arabook/arabook/api/auth/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.arabook.arabook.api.auth.controller;
+
+import static com.arabook.arabook.common.success.member.MemberSuccessType.*;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.arabook.arabook.api.auth.controller.dto.request.AuthRequest;
+import com.arabook.arabook.api.auth.controller.dto.response.AuthResponse;
+import com.arabook.arabook.api.auth.service.AuthService;
+import com.arabook.arabook.common.response.ResponseTemplate;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController implements AuthApi {
+  private final AuthService authService;
+
+  @PostMapping("/social-login")
+  public ResponseEntity<ResponseTemplate<AuthResponse>> signUpOrLogin(
+      @RequestBody AuthRequest request) {
+    AuthResponse response = authService.signUpOrLogin(request);
+    return ResponseEntity.ok(ResponseTemplate.success(ONBOARDING_SUCCESS, response));
+  }
+}
