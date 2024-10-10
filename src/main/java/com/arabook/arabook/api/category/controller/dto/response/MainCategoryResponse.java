b6c7e7096d51f5e479d8c8731e7842e9b22daf98
sung-silver <jinna0319@gmail.com>
2024-10-11 00:23:40 +0900

    [Feat] #58 - 책 대분류 카테고리 조회 API 명세서 작성

---
+++ src/main/java/com/arabook/arabook/api/category/controller/dto/response/MainCategoryResponse.java
@@ -0,0 +1,8 @@
+package com.arabook.arabook.api.category.controller.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(name = "MainCategoryResponse", description = "대분류 카테고리 응답 DTO")
+public record MainCategoryResponse(
+    @Schema(description = "대분류 카테고리 id", example = "1") long mainCategoryId,
+    @Schema(description = "대분류 카테고리명", example = "로맨스") String mainCategoryName) {}
