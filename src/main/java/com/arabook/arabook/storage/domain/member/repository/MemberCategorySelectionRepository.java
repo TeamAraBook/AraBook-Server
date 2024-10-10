ebaa2736e58a2ddfb7216a2eac9def36003a719d
sung-silver <jinna0319@gmail.com>
2024-10-10 15:14:35 +0900

    [Feat] #55 - MemberCategorySelections -> MemberSubCategorySelections로 변경

--- src/main/java/com/arabook/arabook/storage/domain/member/repository/MemberCategorySelectionRepository.java
+++ src/main/java/com/arabook/arabook/storage/domain/member/repository/MemberCategorySelectionRepository.java
@@ -3,9 +3,9 @@ package com.arabook.arabook.storage.domain.member.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.arabook.arabook.storage.domain.member.entity.Member;
-import com.arabook.arabook.storage.domain.member.entity.MemberCategorySelection;
+import com.arabook.arabook.storage.domain.member.entity.MemberSubCategorySelection;
 
 public interface MemberCategorySelectionRepository
-    extends JpaRepository<MemberCategorySelection, Long> {
+    extends JpaRepository<MemberSubCategorySelection, Long> {
   void deleteAllByMember(Member member);
 }
