9093e71b628bb81d2c6baaf80a42e26553ea9e4f
sung-silver <jinna0319@gmail.com>
2024-10-10 15:09:56 +0900

    [Feat] #55 - MainCategroy 생성

---
+++ src/main/java/com/arabook/arabook/storage/domain/category/entity/MainCategory.java
@@ -0,0 +1,24 @@
+package com.arabook.arabook.storage.domain.category.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "main_categories")
+public class MainCategory {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long mainCategoryId;
+
+  @NotNull private String mainCategoryName;
+}
