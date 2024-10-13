7c57f59a627398091cf026c69b389fc83ff69e23
sung-silver <jinna0319@gmail.com>
2024-10-13 16:37:46 +0900

    [Feat] #57 - 필드에 대한 설명을 example이 아닌 description으로 위치 이동

--- src/main/java/com/arabook/arabook/api/member/controller/dto/request/MemberOnboardingRequest.java
+++ src/main/java/com/arabook/arabook/api/member/controller/dto/request/MemberOnboardingRequest.java
@@ -12,6 +12,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 public record MemberOnboardingRequest(
     @Schema(description = "회원의 별명", example = "으나") @NotNull String nickname,
     @Schema(description = "성별 (MAN, WOMAN, UNKNOWN", example = "WOMAN") @NotNull Gender gender,
-    @Schema(description = "태어난 년도", example = "2001, 선택하지 않은 경우에는 null") String birthYear,
+    @Schema(description = "태어난 년도, 선택하지 않은 경우에는 null", example = "2001") String birthYear,
     @Schema(description = "서버로부터 조회한 sub category id 리스트", example = "[1,2,3]") @NotNull
         List<Long> interestSubCategoryIds) {}
