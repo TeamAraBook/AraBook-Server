fea56f2e805ee71378b38e5d70621cfe61739d8e
sung-silver <jinna0319@gmail.com>
2024-10-13 15:37:48 +0900

    [Feat] #59 - SubCategoryCustomRepository 구현체 SubCategoryCustomRepositoryImpl 구현

---
+++ src/main/java/com/arabook/arabook/storage/domain/category/repository/SubCategoryCustomRepositoryImpl.java
@@ -0,0 +1,36 @@
+package com.arabook.arabook.storage.domain.category.repository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.arabook.arabook.storage.domain.category.entity.QMainCategory;
+import com.arabook.arabook.storage.domain.category.entity.QSubCategory;
+import com.querydsl.core.Tuple;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class SubCategoryCustomRepositoryImpl implements SubCategoryCustomRepository {
+  private final JPAQueryFactory queryFactory;
+
+  public List<Tuple> findSubCategoriesByMainCategories(List<Long> mainCategoryIds) {
+    QMainCategory mainCategory = QMainCategory.mainCategory;
+    QSubCategory subCategory = QSubCategory.subCategory;
+
+    return queryFactory
+        .select(
+            mainCategory.mainCategoryId,
+            mainCategory.mainCategoryName,
+            subCategory.subCategoryId,
+            subCategory.subCategoryName)
+        .from(mainCategory)
+        .leftJoin(subCategory)
+        .on(subCategory.mainCategory.mainCategoryId.eq(mainCategory.mainCategoryId))
+        .where(mainCategory.mainCategoryId.in(mainCategoryIds))
+        .orderBy(mainCategory.mainCategoryName.asc(), subCategory.subCategoryName.asc())
+        .fetch();
+  }
+}
