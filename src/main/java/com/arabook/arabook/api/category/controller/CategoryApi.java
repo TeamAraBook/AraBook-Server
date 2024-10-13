6ed58ad1ed981f2172eaa76fb60620331cff91f1
sung-silver <jinna0319@gmail.com>
2024-10-13 14:51:09 +0900

    [Feat] #59 - 대분류 카테고리 응답 형식 변경 및 대분류에 따른 소분류 카테고리 조회에 대한 API 명세서 작성

--- src/main/java/com/arabook/arabook/api/category/controller/CategoryApi.java
+++ src/main/java/com/arabook/arabook/api/category/controller/CategoryApi.java
@@ -1,8 +1,11 @@
 package com.arabook.arabook.api.category.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 
-import com.arabook.arabook.api.category.controller.dto.response.MainCategoriesResponse;
+import com.arabook.arabook.api.category.controller.dto.response.MainCategoryResponse;
+import com.arabook.arabook.api.category.controller.dto.response.MainCategoryWithSubCategoriesResponse;
 import com.arabook.arabook.common.response.ResponseTemplate;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -18,15 +21,32 @@ public interface CategoryApi {
       value = {
         @ApiResponse(
             responseCode = "200",
-            description = "카테고리 조회에 성공했습니다.",
+            description = "대분류 카테고리 조회에 성공했습니다.",
             content =
                 @Content(
                     mediaType = "application/json",
                     schema =
                         @Schema(
                             example =
-                                "{\"code\": 200, \"message\": \"대분류 카테고리 조회에 성공했습니다\", \"data\": { \"totalCount\": 2, \"mainCategories\": [ { \"mainCategoryId\": 1, \"mainCategoryName\": \"로맨스\" }, { \"mainCategoryId\": 2, \"mainCategoryName\": \"판타지\" }]}}")))
+                                "{\"code\": 200, \"message\": \"대분류 카테고리 조회에 성공했습니다\", \"data\": { [ { \"mainCategoryId\": 1, \"mainCategoryName\": \"로맨스\" }, { \"mainCategoryId\": 2, \"mainCategoryName\": \"판타지\" } ] }")))
       })
   @Operation(summary = "온보딩: 대분류 카테고리 리스트 조회", description = "책 대분류 카테고리를 조회합니다.")
-  ResponseEntity<ResponseTemplate<MainCategoriesResponse>> getMainCategories();
+  ResponseEntity<ResponseTemplate<List<MainCategoryResponse>>> getMainCategories();
+
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "대분류 카테고리별 소분류 카테고리 리스트 조회에 성공했습니다.",
+            content =
+                @Content(
+                    mediaType = "application/json",
+                    schema =
+                        @Schema(
+                            example =
+                                "{\"code\": 200, \"message\": \"대분류 카테고리별 소분류 카테고리 리스트 조회에 성공했습니다\", \"data\": { \"totalCount\": 2, \"mainCategories\": [ { \"mainCategoryId\": 1, \"mainCategoryName\": \"로맨스\" }, { \"mainCategoryId\": 2, \"mainCategoryName\": \"판타지\" }]}}")))
+      })
+  @Operation(summary = "온보딩: 대분류 카테고리 리스트 조회", description = "책 대분류 카테고리를 조회합니다.")
+  ResponseEntity<ResponseTemplate<List<MainCategoryWithSubCategoriesResponse>>>
+      getSubCategoriesByMainCategories(List<Long> mainCategoryIds);
 }
---
+++ src/main/java/com/arabook/arabook/api/category/controller/dto/response/MainCategoryWithSubCategoriesResponse.java
@@ -0,0 +1,19 @@
+package com.arabook.arabook.api.category.controller.dto.response;
+
+import java.util.List;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(
+    name = "MainCategoryWithSubCategoriesResponse",
+    description = "대분류 카테고리별 소분류 리스트 카테고리 응답 DTO")
+public record MainCategoryWithSubCategoriesResponse(
+    @Schema(description = "대분류 카테고리 id", example = "1") long mainCategoryId,
+    @Schema(description = "대분류 카테고리 이름", example = "소설") String mainCategoryName,
+    @Schema(description = "해당 대분류의 소분류 카테고리 리스트") List<SubCategoryResponse> subCategories) {
+  public static MainCategoryWithSubCategoriesResponse of(
+      long mainCategoryId, String mainCategoryName, List<SubCategoryResponse> subCategories) {
+    return new MainCategoryWithSubCategoriesResponse(
+        mainCategoryId, mainCategoryName, subCategories);
+  }
+}
