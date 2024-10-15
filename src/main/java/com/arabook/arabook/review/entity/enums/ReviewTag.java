c2b1076cf01bca00488701ccd98cb39785c5798d
sung-silver <jinna0319@gmail.com>
2024-10-15 21:53:58 +0900

    [Chore] #66 - 카드 이미지 위치 변경에 따른 url 변경

--- src/main/java/com/arabook/arabook/review/entity/enums/ReviewTag.java
+++ src/main/java/com/arabook/arabook/review/entity/enums/ReviewTag.java
@@ -7,23 +7,23 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ReviewTag {
   DISAPPOINTED(
-      "https://github.com/TeamAraBook/AraBook-Server/blob/dev/images/card/DISAPPOINTED.png?raw=true",
+      "https://github.com/TeamAraBook/AraBook-Server/blob/dev/src/main/resources/images/card/DISAPPOINTED.png?raw=true",
       "별로였어...",
       "#FFD600"),
   SLIGHTLY_DISAPPOINTED(
-      "https://github.com/TeamAraBook/AraBook-Server/blob/dev/images/card/SLIGHTLY_DISAPPOINTED.png?raw=true",
+      "https://github.com/TeamAraBook/AraBook-Server/blob/dev/src/main/resources/images/card/SLIGHTLY_DISAPPOINTED.png?raw=true",
       "조금 아쉬웠어..",
       "#FF8339"),
   AVERAGE(
-      "https://github.com/TeamAraBook/AraBook-Server/blob/dev/images/card/AVERAGE.png?raw=true",
+      "https://github.com/TeamAraBook/AraBook-Server/blob/dev/src/main/resources/images/card/AVERAGE.png?raw=true",
       "평범했어.",
       "#7187FF"),
   ENJOYABLE(
-      "https://github.com/TeamAraBook/AraBook-Server/blob/dev/images/card/ENJOYABLE.png?raw=true",
+      "https://github.com/TeamAraBook/AraBook-Server/blob/dev/src/main/resources/card/ENJOYABLE.png?raw=true",
       "재밌었어!",
       "#1FD068"),
   LIFE_CHANGING(
-      "https://github.com/TeamAraBook/AraBook-Server/blob/dev/images/card/LIFE_CHANGING.png?raw=true",
+      "https://github.com/TeamAraBook/AraBook-Server/blob/dev/src/main/resources/card/LIFE_CHANGING.png?raw=true",
       "내 인생책이야 !!",
       "#FE5D5C");
 
