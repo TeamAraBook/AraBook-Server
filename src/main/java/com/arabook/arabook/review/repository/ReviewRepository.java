d43643f8d333254e2e8ff48268ca0d4764a93f8b
sung-silver <jinna0319@gmail.com>
2024-10-30 23:26:38 +0900

    [Feat] #76 - ReviewRepository에 deleteByReviewer 정의

--- src/main/java/com/arabook/arabook/review/repository/ReviewRepository.java
+++ src/main/java/com/arabook/arabook/review/repository/ReviewRepository.java
@@ -4,11 +4,12 @@ import static com.arabook.arabook.common.exception.review.ReviewExceptionType.*;
 
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.*;
 
 import com.arabook.arabook.common.exception.review.ReviewException;
+import com.arabook.arabook.member.entity.*;
 import com.arabook.arabook.review.entity.Review;
 
 public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewCustomRepository {
@@ -21,4 +22,11 @@ public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewCus
     return findByReviewIdAndReviewerId(reviewId, reviewerId)
         .orElseThrow(() -> new ReviewException(NOT_FOUND_REVIEW));
   }
+
+  @Modifying(clearAutomatically = true)
+  @Transactional
+  @Query("delete from Review r where r.reviewer =: reviewer")
+  void deleteByReviewer(@Param("reviewer") Member reviewer);
+
+  void deleteAllByReviewer(final Member member);
 }
